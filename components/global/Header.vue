1e5685e2ee0560a4abcc102e12bf32b3e27d6641
ptrlawless <59983278+ptrlawless@users.noreply.github.com>
2021-09-04 13:47:37 -0700

    stuff

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -2,13 +2,16 @@
   <nav class="scrim-bg fixed z-40 top-0 inset-x-0 pt-3 px-3" aria-label="Main Menu">
     <ul class="flex">
       <li class="flex-1">
-        <nuxt-link class="btn block" to="/">Home</nuxt-link>
+        <nuxt-link class="flex justify-center items-center h-full" to="/">Home</nuxt-link>
       </li>
       <li class="flex-1 ml-2">
-        <nuxt-link class="btn block" to="/blog">Blog</nuxt-link>
+        <nuxt-link class="flex justify-center items-center h-full" to="/blog">Blog</nuxt-link>
       </li>
       <li class="flex-1 ml-2">
-        <nuxt-link class="btn block" to="/projects">Projects</nuxt-link>
+        <nuxt-link class="flex justify-center items-center h-full" to="/projects">Projects</nuxt-link>
+      </li>
+      <li class="flex-1 ml-2">
+        <nuxt-link class="flex justify-center items-center h-full" to="/projects">Projects</nuxt-link>
       </li>
     </ul>
   </nav>
@@ -16,7 +19,7 @@
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
 }
 </script>
 
@@ -55,11 +58,19 @@ export default {
 
 /* Need two because of smoother switching between color modes */
 @keyframes fadeIn1 {
-  from { opacity: 0; }
-  to { opacity: 1; }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 @keyframes fadeIn2 {
-  from { opacity: 0; }
-  to { opacity: 1; }
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
 }
 </style>
